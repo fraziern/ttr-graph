7d95aee5020850cece5d6cd510f648b3299f2894
Nick Frazier <nrflaw@gmail.com>
2017-05-21 08:08:49 -0400

    Snap debugging

--- public/index.js
+++ public/index.js
@@ -1,25 +1,89 @@
+/*global mina Snap*/
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+var pathList = [];
+var answerList = [];
+
 function calculateRoute(e) {
   e.preventDefault();
 
   // build JSON
   let routes = pathList.map((path) => {
-    let cities = path.split("-");
+    let cities = path.replace(/_/g," ").split("-");
     if (cities.length !== 2) {
-      console.log("Cities count wrong");
+      console.error("Cities count wrong");
     }
     else return {
-      "node1": cities[0].replace("_"," "),
-      "node2": cities[1].replace("_"," ")
+      "node1": cities[0],
+      "node2": cities[1]
     };
   });
 
   let JSONOut = { "routes": routes };
-  console.log(JSON.stringify(JSONOut));
   // send JSON
-
+  fetch("/api/longest", {
+    method: "POST",
+    body: JSON.stringify(JSONOut),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  }).then((json) => json.json())
+  .then((data) => {
+    displayAnswer(data);
+  }).catch((err) => {
+    console.error(err);
+  });
   // get response, display it
 }
 
+function resetGraph() {
+  // use pubsub
+}
+
+function displayAnswer(data) {
+  pubSub.publish("updateDisplay", "Longest Length: " + data.length);
+
+  // generate id names
+  answerList = [];
+  for (let i = 1; i < data.trail.length; i++) {
+    let nodes = [data.trail[i-1], data.trail[i]].sort();
+    // TODO change this to a pubsub call
+    // that both adds to the answerlist and updates the display
+    answerList.push(nodes.join("-").replace(/\s/g, "_"));
+  }
+
+  // toggle classes to display answer
+  answerList.forEach((nodeName, idx) => {
+    let node = document.querySelector("#" + nodeName);
+    window.setTimeout(() => {
+      animatePath("#" + nodeName);
+      node.classList.add("answer");
+      node.classList.remove("on");
+    }, 500 * idx);
+  });
+}
+
+function animatePath(gid) {
+  let s = Snap(gid);
+  let children = s.children();
+
+  for (let i = 0; i < children.length; i++) {
+    if (children[i].type == "rect") {
+      let box = children[i].getBBox();
+      // s.rect(box.x,box.y,box.w, box.h);   // check bounding box
+      let matrixStart = children[i].transform().localMatrix;
+      let matrixEnd = matrixStart.clone();
+      children[i].animate({
+        transform: "s1.3 " + matrixEnd.toTransformString()
+      }, 200 + 200*i, mina.easeout, () => {
+        children[i].animate({
+          transform: matrixStart.toTransformString()
+        }, 1000);
+      });
+    }
+  }
+}
+
+// using pubSub for 2 way data binding
 var pubSub = (function() {
   var callbacks = {};
 
@@ -36,8 +100,7 @@ var pubSub = (function() {
   };
 })();
 
-var pathList = [];
-
+// TODO: should this use a set instead?
 var listToggle = (nodeName) => {
   let idx = pathList.indexOf(nodeName);
   if (idx >= 0) pathList.splice(idx, 1);
@@ -51,9 +114,9 @@ pubSub.on("toggle", (nodeName) => {
   listToggle(nodeName);
 });
 
-pubSub.on("updateDisplay", () => {
+pubSub.on("updateDisplay", (content) => {
   let output = document.querySelector(".output-text");
-  output.textContent = pathList;
+  output.textContent = content;
 });
 
 var components = document.querySelectorAll("#svg2 > *");
